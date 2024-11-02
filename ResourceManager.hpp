bd159bdd81cf85f2edc4ac1ddbac12cdf79b50f9
lukaszk22 <lukaszkicinski22@gmail.com>
2024-11-02 18:37:04 +0100

    zejsciowka nr 2

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,52 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+    private:
+
+    Resource* resource_ = nullptr;                                  // Zarządzany obiekt
+    double out;                                                     // Zwracany wynik metody get()
+
+    public:
+    ResourceManager();                                              // Konstruktor domyslny
+    ~ResourceManager();                                             // Destruktor
+    ResourceManager(const ResourceManager& resource);               // Konstruktor kopiujacy
+    ResourceManager& operator=(const ResourceManager& resource);    // Kopiujący operator przypisania
+
+    double get();                                                   // metoda get
 };
+
+ResourceManager::ResourceManager() 
+{
+    resource_ = new Resource{};
+    out = resource_->get();
+}
+
+ResourceManager::~ResourceManager()
+{
+    delete resource_;
+}
+
+ResourceManager::ResourceManager(const ResourceManager& resource)
+{
+    resource_ = new Resource{};
+    out = resource.out;
+}
+
+ResourceManager& ResourceManager::operator=(const ResourceManager& resource)
+{
+
+    if(this == &resource){
+        return *this;
+    }
+
+    delete resource_;
+    resource_ = new Resource{};
+    out = resource.out;
+
+    return *this;
+}
+
+double ResourceManager::get()
+{
+    return out; 
+}
